2a0d69492bfd437e37cebc3a5c7c8c0da0b46136
lRodz <98000915+lRodz@users.noreply.github.com>
2023-04-25 12:36:18 -0300

    Update Exercise 1.java

--- Exercise 1.java
+++ Exercise 1.java
@@ -1,4 +1,4 @@
-//Write a Java program to print 'Hello' on screen and your name on a separate line
+//Write a Java program to print 'Hello' on screen and your name on a separate line.
 
 package com.mycompany.exercises;
 import java.util.Scanner;
