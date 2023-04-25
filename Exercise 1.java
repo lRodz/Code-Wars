82cf54899864973c25e110ead653d08fdb3c2b1f
lRodz <98000915+lRodz@users.noreply.github.com>
2023-04-25 12:35:51 -0300

    Update Exercise 1.java

--- Exercise 1.java
+++ Exercise 1.java
@@ -1,3 +1,5 @@
+//Write a Java program to print 'Hello' on screen and your name on a separate line
+
 package com.mycompany.exercises;
 import java.util.Scanner;
 
@@ -5,8 +7,6 @@ public class Exercises {
     
     public static void main(String[] args) {
 
-        //Write a Java program to print 'Hello' on screen and your name on a separate line
-        
          Scanner scanner = new Scanner(System.in);
          
          System.out.println("What is your name?");
