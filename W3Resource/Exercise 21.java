6f11a7616cc3ae6ae349ccc029cdb3034a061533
lRodz <98000915+lRodz@users.noreply.github.com>
2023-04-30 19:33:12 -0300

    Add files via upload

---
+++ W3Resource/Exercise 21.java
@@ -0,0 +1,48 @@
+//Write a Java program to print the ASCII value of a given character.
+
+package com.mycompany.exercises;
+import java.util.Scanner;
+
+public class Exercises {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+	       System.out.println("What is the Character?");
+                          int character = scanner.next().charAt(0);
+                          
+                          System.out.println(character);
+        
+       
+                    }
+                }
+            
+            
+            
+        
+           
+
+
+    
+
+                        
+                    
+        
+
+
+	
+
+	
+
+        
+        
+         
+          
+    
+
+        
+    
+    
+
+
